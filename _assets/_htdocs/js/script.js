a767161411aaedbecc8c421af7b67f09d07c17fc
yusasa16 <yusasa16@gmail.com>
2022-06-05 19:33:33 +0900

    vuex導入

--- _assets/_htdocs/js/script.js
+++ _assets/_htdocs/js/script.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
 import 'normalize.css'
 import '../css/style.scss'
 import vHeader from '../../inc/js/vue/vHeader.vue'
@@ -8,11 +9,85 @@ import vWorks from '../../inc/js/vue/vWorks.vue'
 import vNews from '../../inc/js/vue/vNews.vue'
 
 window.addEventListener('load', function() {
-	const hamburgerElem = $(".header-inner__hamburger");
-	hamburgerElem.on('click', () => {
-		hamburgerElem.toggleClass('on');
-		$(".header-modal").toggleClass('on');
-	});
+	const store = createStore({
+		state() {
+			return {
+				news: [
+					{
+						href: '__URL__',
+						date: {
+							year: '2020',
+							month: 'XX',
+							date: 'xx'
+						},
+						title: 'デザイン雑誌「ＸＸＸＸＸＸ Vol.11』に掲載していただきました。'
+					},
+					{
+						href: '__URL__',
+						date: {
+							year: '2020',
+							month: 'XX',
+							date: 'xx'
+						},
+						title: 'ＸＸ月ＸＸ日から写真集「ＸＸＸＸＸＸＸ」の販売を開始します。'
+					},
+					{
+						href: '__URL__',
+						date: {
+							year: '2020',
+							month: 'XX',
+							date: 'xx'
+						},
+						title: '【イベント開催のお知らせ】テキストテキストテキストテキストテキストテキストテキスト'
+					},
+					{
+						href: '__URL__',
+						date: {
+							year: '2020',
+							month: 'XX',
+							date: 'xx'
+						},
+						title: 'デザイン雑誌「ＸＸＸＸＸＸ Vol.10』に掲載していただきました。'
+					},
+					{
+						href: '__URL__',
+						date: {
+							year: '2020',
+							month: 'XX',
+							date: 'xx'
+						},
+						title: '【個展開催のお知らせ】テキストテキストテキストテキストテキストテキストテキスト'
+					},
+				],
+				works: [
+					{
+						src: '/img/common/pict-works01.jpg',
+						alt: 'works01 画像'
+					},
+					{
+						src: '/img/common/pict-works02.jpg',
+						alt: 'works02 画像'
+					},
+					{
+						src: '/img/common/pict-works03.jpg',
+						alt: 'works03 画像'
+					},
+					{
+						src: '/img/common/pict-works04.jpg',
+						alt: 'works04 画像'
+					},
+					{
+						src: '/img/common/pict-works05.jpg',
+						alt: 'works05 画像'
+					},
+					{
+						src: '/img/common/pict-works06.jpg',
+						alt: 'works06 画像'
+					},
+				]
+			}
+		}
+	})
 
 	createApp(vHeader).mount('#header');
 	createApp(vFooter).mount('#footer');
@@ -23,5 +98,5 @@ window.addEventListener('load', function() {
 			'v-works': vWorks,
 			'v-news': vNews
 		}
-	}).mount('#main');
+	}).use(store).mount('#main');
 })
--- _assets/inc/js/vue/vWorks.vue
+++ _assets/inc/js/vue/vWorks.vue
@@ -9,32 +9,7 @@
 export default {
 	data() {
 		return {
-			works: [
-				{
-					src: '/img/common/pict-works01.jpg',
-					alt: 'works01 画像'
-				},
-				{
-					src: '/img/common/pict-works02.jpg',
-					alt: 'works02 画像'
-				},
-				{
-					src: '/img/common/pict-works03.jpg',
-					alt: 'works03 画像'
-				},
-				{
-					src: '/img/common/pict-works04.jpg',
-					alt: 'works04 画像'
-				},
-				{
-					src: '/img/common/pict-works05.jpg',
-					alt: 'works05 画像'
-				},
-				{
-					src: '/img/common/pict-works06.jpg',
-					alt: 'works06 画像'
-				},
-			]
+			works: this.$store.state.works
 		}
 	},
 }
