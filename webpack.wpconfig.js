8fde8056e2283ec414a28a83e74d76b1bcb10b90
yusasa16 <yusasa16@gmail.com>
2022-06-08 00:35:46 +0900

    wordpress用ファイル吐き出し

---
+++ webpack.wpconfig.js
@@ -0,0 +1,108 @@
+const path = require('path');
+const glob = require('glob');
+// const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+// const htmlList = require('./_webpack-libs/gethtml');
+const { VueLoaderPlugin } = require('vue-loader');
+const outputPath = path.resolve(__dirname, 'wordpress/wp-content/themes/original');
+
+const setting = {
+	entry: './_assets/_htdocs/js/script.js',
+	output: {
+		path: outputPath,
+		filename: 'js/script.js',
+	},
+	devtool: "source-map",
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env']
+						}
+					},
+				],
+			},
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader'
+			},
+			{
+				test: /\.(scss|css)$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							url: false,
+						},
+					},
+					'postcss-loader',
+					{
+						loader: "sass-loader",
+						options: {
+							sourceMap: true,
+						},
+					},
+				],
+			},
+			{
+				test: /\.html?$/,
+				use: [
+					{
+						loader: 'html-loader',
+						options: {
+							sources: false,
+						},
+					},
+					{
+						loader: 'webpack-ssi-include-loader',
+						options: {
+							localPath: path.join(__dirname, '/_assets'),
+							location: 'https://your.website.com/', // http url where the file can be dl
+						},
+					},
+				],
+			},
+		],
+	},
+
+	resolve: {
+		alias: {
+			vue$: 'vue/dist/vue.esm-bundler.js',
+		},
+	},
+
+	plugins: [
+		new MiniCssExtractPlugin({
+				filename: 'css/style.css',
+		}),
+
+		new CopyPlugin({
+			patterns: [
+				{
+					from: './_assets/_htdocs/img',
+					to: `${outputPath}/img`
+				}
+			]
+		}),
+
+		// new BrowserSyncPlugin({
+		// 		host: 'localhost',
+		// 		port: 3000,
+		// 		server: { baseDir: outputPath }
+		// }),
+
+		new VueLoaderPlugin()
+	],
+
+	target: 'web',
+};
+
+module.exports = setting;
